483ef7d61825b3d2847573b7443a798393115eeb
Aditya Mullick <adityamullick98@yahoo.com>
2016-09-21 09:38:31 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+
+	private int x;
+	public Hello() {
+		// TODO Auto-generated constructor stub
+		x = 3;
+		
+	}
+	public void getX()
+	{
+		System.out.println(x);
+		
+	}
+
+
+}
